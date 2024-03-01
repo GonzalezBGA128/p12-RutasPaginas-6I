92225469a1ad3ed8be08d94243ff523109b549f2
GonzalezBGA128 <144726562+GonzalezBGA128@users.noreply.github.com>
2024-03-01 13:30:38 -0700

    Update pantalla1.dart

--- lib/pantalla1.dart
+++ lib/pantalla1.dart
@@ -10,14 +10,42 @@ class Pantalla1 extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Pantalla 1 Grecia Gonzalez'),
       ),
-      body: Center(
-          child: ElevatedButton(
-        child: const Text("Vamos a la pantalla 2"),
-        onPressed: () {
-          Navigator.pushNamed(context, "/pantalla2",
-              arguments: "Mensaje desde la pantalla 1");
-        },
-      )),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Align(
+            alignment: Alignment.center,
+          ),
+          Text(
+            "BIENVENIDO!",
+            style: TextStyle(
+              fontSize: 60,
+              color: Color(0xff3a0044),
+            ),
+          ),
+          Icon(Icons.home, size: 300, color: Colors.purple),
+          ElevatedButton(
+            child: const Text("Vamos a la pantalla 2",
+                style: TextStyle(
+                  fontSize: 25,
+                )),
+            onPressed: () {
+              Navigator.pushNamed(context, "/pantalla2",
+                  arguments: "Mensaje desde la pantalla 1");
+            },
+          ),
+          Card(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                "Tarjeta Gonzalez",
+                style: TextStyle(fontSize: 30, color: Color(0xff3a0044)),
+              ),
+            ),
+            color: Color(0xffda7eea),
+          )
+        ],
+      ),
     );
   } //widget
 } //fin clase pantalla 1
