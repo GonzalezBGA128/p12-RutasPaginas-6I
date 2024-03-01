0fc0a889692710d672f29920673f575ac0ca7f00
GonzalezBGA128 <144726562+GonzalezBGA128@users.noreply.github.com>
2024-03-01 13:31:01 -0700

    Update pantalla2.dart

--- lib/pantalla2.dart
+++ lib/pantalla2.dart
@@ -16,11 +16,27 @@ class Pantalla2 extends StatelessWidget {
           children: [
             Text(argumento),
             ElevatedButton(
-              child: const Text("Regresar a Pantalla 1"),
+              child: const Text("Regresar a Pantalla 1",
+                  style: TextStyle(fontSize: 35)),
               onPressed: () {
                 Navigator.pop(context);
               },
-            )
+            ),
+            Container(
+              width: double.infinity,
+              height: 250,
+              child: Card(
+                color: Color(0xfff97ca6),
+                margin: EdgeInsets.all(30),
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    'Bienvenido a la pantalla 2',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
